43a6e3f1881bd8f8adb21a675f39130712b62af6
Maksim <mirror0791@mail.ru>
2024-02-07 19:31:42 +0300

    Feature commit 4

--- src/drugstore/Main.java
+++ src/drugstore/Main.java
@@ -12,25 +12,29 @@ public class Main {
         Component water = new Water("Water", 10D, 1);
         Component azitronite = new Azitronite("Azitronite", 2D, 14);
         Component penicillin = new Penicillin("Penicillin", 1.6D, 6);
+        Component vitasporin = new Penicillin("Vitasporin", 1.1D, 17);
 
         List<Pharmacy> pharmacyList = new ArrayList<>();
-        Pharmacy newPenicilin = new Pharmacy("Penicilin");
-        newPenicilin.addComponents(penicillin,water);
-        pharmacyList.add(newPenicilin);
-        Pharmacy newAzitronite = new Pharmacy("Azitronite");
-        newAzitronite.addComponents(azitronite,water);
-        pharmacyList.add(newAzitronite);
-        Pharmacy newAzitroPenicilin = new Pharmacy("Azitropenicilin");
-        newAzitroPenicilin.addComponents(penicillin,water,azitronite);
-        pharmacyList.add(newAzitroPenicilin);
+
+
+        Pharmacy pharm1 = new Pharmacy("Azitronite");
+        pharm1.addComponents(azitronite,water);
+        pharmacyList.add(pharm1);
+        Pharmacy pharm2 = new Pharmacy("Vitasporin");
+        pharm2.addComponents(vitasporin,water,water);
+        pharmacyList.add(pharm2);
+        Pharmacy pharm3 = new Pharmacy("Penicilin");
+        pharm3.addComponents(penicillin,water);
+        pharmacyList.add(pharm3);
 
         List<Component> components = new ArrayList<>();
         components.add(azitronite);
         components.add(water);
         components.add(penicillin);
+        components.add(vitasporin);
 
-        System.out.println(pharmacyList);
+        System.out.println("До сортировки" + pharmacyList);
         Collections.sort(pharmacyList);
-        System.out.println(pharmacyList);
+        System.out.println("После сортировки" + pharmacyList);
     }
 }
